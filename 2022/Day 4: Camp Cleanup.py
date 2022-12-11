015394d4865bb11c3e5f18818b7947e6fc26ba80
Naomi Josebashvili <96797525+naomiJosebashvili@users.noreply.github.com>
2022-12-11 00:24:16 +0000

    Create Day 4: Camp Cleanup.py

---
+++ 2022/Day 4: Camp Cleanup.py
@@ -0,0 +1,44 @@
+# Part 1:
+# Finds how many pairs of Elves are assigned to clean sections that overlap entirely
+
+file = open("day4InputFile.txt", "r")
+numberOfFullyOverlappedPairs = 0
+
+for pair in file:
+    splitPairs = pair.split(",")
+    elf1 = splitPairs[0].split("-")
+    elf2 = splitPairs[1].split("-")
+    elf1LowerBound = int(elf1[0])
+    elf1UpperBound = int(elf1[1])
+    elf2LowerBound = int(elf2[0])
+    elf2UpperBound = int(elf2[1])
+    if elf1LowerBound >= elf2LowerBound and elf1UpperBound <= elf2UpperBound or elf1LowerBound <= elf2LowerBound and elf2UpperBound <= elf1UpperBound:
+        numberOfFullyOverlappedPairs += 1
+        
+file.close()
+print(numberOfFullyOverlappedPairs)
+
+# Part 2:
+# Finds how many pairs of Elves are assigned to clean sections that overlap
+
+def isOverlapping(elf1, elf2):
+    elf1LowerBound = int(elf1[0])
+    elf1UpperBound = int(elf1[1])
+    elf2LowerBound = int(elf2[0])
+    elf2UpperBound = int(elf2[1])
+    if (elf1LowerBound >= elf2LowerBound and elf1LowerBound <= elf2UpperBound) or (elf1UpperBound >= elf2LowerBound and elf1UpperBound <= elf2UpperBound):
+        return True
+    return False
+
+file = open("day4InputFile.txt", "r")
+numberOfOverlappedPairs = 0
+    
+for pair in file:
+    splitPairs = pair.split(",")
+    elf1 = splitPairs[0].split("-")
+    elf2 = splitPairs[1].split("-")
+    if isOverlapping(elf1, elf2) or isOverlapping(elf2, elf1):
+        numberOfOverlappedPairs += 1
+        
+file.close()
+print(numberOfOverlappedPairs)
